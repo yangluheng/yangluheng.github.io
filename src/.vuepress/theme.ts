fdd0165f668601402e636566376dc92e51abd288
Hans Young <ylh310508863@outlook.com>
2023-12-22 15:07:38 +0800

    45

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -48,7 +48,7 @@ export default hopeTheme({
       // sidebar
       sidebar: zhSidebar,
 
-      footer: "© 2023-present 鲁ICP备"<a href="https://beian.miit.gov.cn" target="_blank"> 19028049</a>"号",
+      footer: <a href="https://beian.miit.gov.cn" target="_blank">© 2023-present 鲁ICP备19028049号</a>,
 
       displayFooter: true,
 
